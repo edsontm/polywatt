ce3230927f3226d0f4706544ff7eae82d1412f40
edsontm <edsontm@gmail.com>
2017-11-22 22:27:29 -0300

    refactoring

---
+++ preprocess.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import pvddtw
+import sys
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2):
+        print("usage: %s <dir>\n"%(sys.argv[0]))
+        exit(0)
+    else:
+        main_dir = sys.argv[1]
+
+    
+
+
